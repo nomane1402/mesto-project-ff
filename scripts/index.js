609e0c34249598c922fac645820657be26f67b27
Artem Khudyak PC <skyluck2013@yandex.ru>
2025-03-16 12:04:16 +1000

    lets start

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content; // получаем содержимое темплейта
+
+const cardElement = cardTemplate.querySelector('.places__item card').cloneNode(true); // клонированируем элемент темплейта
+
 // @todo: DOM узлы
+
+const content = document.querySelector('.content');
+
+
+const imageElement = document.createElement('img');
+imageElement.classList.add('card__image');
+
+const deleteButtonElement = document.createElement('button');
+deleteButtonElement.classList.add('card__delete-button');
+
+const titleElement = document.createElement('h2');
+titleElement.classList.add('card__title');
+
+const likeButtonElement = document.createElement('button');
+likeButtonElement.classList.add('card__like-button');
+
 
 // @todo: Функция создания карточки
 
+function createCard() {
+
+}
+
 // @todo: Функция удаления карточки
+
+
 
 // @todo: Вывести карточки на страницу
