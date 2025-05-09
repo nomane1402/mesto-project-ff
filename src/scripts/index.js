54b29a04d0350031df88646d33a7d49c74c47238
Artem Khudyak PC <skyluck2013@yandex.ru>
2025-05-09 21:10:21 +1000

    end of begining

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -145,3 +145,69 @@ initialCards.forEach(renderCard);
     newCardPopup.addEventListener('submit', handleAddCardFormSubmit);
 
   });
+
+  // ПР7
+
+  const popupForm = document.querySelector('.popup__form');
+  const popupInput = popupForm.querySelector('.popup__input');
+  const popupError = popupForm.querySelector(`.${popupInput.id}-error`);
+
+  const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    formElement.classList.add('popup__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+  };
+  
+  const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    formElement.classList.remove('popup__input_type_error');
+    errorElement.classList.remove('popup__input-error_active')
+    errorElement.textContent = '';
+  };
+
+  const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  };
+
+  // const checkInputValidity = (formElement, inputElement) => {
+  //   if (inputElement.validity.patternMismatch) {
+  //     inputElement.setCustomValidity("Разрешены только латинские и кириллические буквы, знаки дефиса и пробелы");
+  //     showInputError(formElement, inputElement, inputElement.validationMessage);
+  //   } else {
+  //     inputElement.setCustomValidity("");
+  //     hideInputError(formElement, inputElement);
+  //   }
+  // };
+
+  const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+        checkInputValidity(formElement, inputElement);
+      });
+    });
+  };
+
+  const enableValidation = () => {
+    let formList = Array.from(document.querySelectorAll('.popup__form'));
+    formList.forEach((formElement) => {
+      // formElement.addEventListener('submit', (evt) => {
+      //   evt.preventDefault();
+      // });
+      setEventListeners(formElement);
+    });
+  };
+
+  enableValidation({
+    formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+  });
